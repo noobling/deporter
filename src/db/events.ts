0952a885aa8b1557733eff18b16fbe34b4be5bef
Neosh Sheikh <neosh11@gmail.com>
2024-04-11 00:51:59 +1000

    Revert "Realtime messages and notifications"

--- src/db/events.ts
+++ src/db/events.ts
@@ -9,7 +9,6 @@ import {
 } from "../types";
 import { getMongoID } from "../utils/mongo";
 import db from "./db";
-import { sendPushNotification, sendWebsocketNotification, WebsocketEventType } from "../services/notificationService";
 
 const collection = db.collection("event");
 
@@ -44,40 +43,7 @@ async function addMessage(id: string, message: Message) {
   await updateList(id, {
     messages: message,
   });
-
-  const data = await getEvent(id);
-
-  // Add notifications to q
-  if (data) {
-    const participants = [...data.participants, data.created_by];
-    const userIds = participants
-    // const userIds = participants.filter((p) => p !== message.created_by);
-    // send notifications to them all
-
-    const url = `/(event)/chat?id=${data._id}`
-    for (const userId of userIds) {
-      sendWebsocketNotification(userId, {
-        type: WebsocketEventType.ROUTING_NOTIFICATION,
-        payload: {
-          goTo: url,
-          title: "New message",
-          description: `${message.created_by} added a new message`,
-        },
-      });
-      sendPushNotification(userId, {
-        type: WebsocketEventType.ROUTING_NOTIFICATION,
-        payload: {
-          goTo: url,
-          title: "New message",
-          description: `${message.created_by} added a new message`,
-        },
-      });
-    }
-
-  }
-
-
-  return data
+  return getEvent(id);
 }
 
 async function addExpense(id: string, expense: Expense) {
