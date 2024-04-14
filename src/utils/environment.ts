50a2f297573ad946e8e43afdddba3a7e9f7dead7
David Yu <dyu5@atlassian.com>
2024-04-14 22:02:40 +1000

    add db as env

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -16,4 +16,5 @@ export default {
   aws_key: process.env.AWS_KEY,
   aws_bucket: process.env.AWS_BUCKET,
   bypass_auth_user_id: process.env.BYPASS_AUTH_USER_ID, // Only for testing should this exist
+  mongo_db_name: process.env.MONGO_DB_NAME,
 };
