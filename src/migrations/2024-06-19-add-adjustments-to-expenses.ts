aba0d8ed3ac920338cfeae09b64d7e9ba0d9e4f5
Neosh <neosh11@gmail.com>
2024-06-19 16:32:34 +1000

    migrate to support adjustments

---
+++ src/migrations/2024-06-19-add-adjustments-to-expenses.ts
@@ -0,0 +1,36 @@
+import { ObjectId } from "mongodb";
+import db from "../db/db";
+import { Expense } from "../types";
+
+export async function addExpenseAdjustments() {
+  // timeout for 5 seconds
+  setTimeout(async () => {
+    const events = db.collection("event");
+    const c = events.find()
+    while (await c.hasNext()) {
+      const event = await c.next();
+      if (!event) {
+        break;
+      }
+      console.log("Event", event._id);
+      // update each expense that doesn't have an id
+      event.expenses = event.expenses.map((expense: Expense) => {
+        if (expense.adjustments === undefined) {
+          expense.adjustments = {};
+        }
+        return expense;
+      });
+      await events.updateOne(
+        { _id: event._id },
+        {
+          $set: {
+            expenses: event.expenses,
+          },
+        }
+      );
+    }
+    process.exit(0);
+  }, 5000);
+}
+
+// addExpenseAdjustments();
